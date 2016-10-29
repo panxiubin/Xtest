c62df76d5a18dadd36dc3b051e1b0a0b4df46cb7
panxiubin <xiubinpan@gmail.com>
2016-10-29 22:47:34 +0800

    json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :courses
+  resources :teachers
+  resources :students
   root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
