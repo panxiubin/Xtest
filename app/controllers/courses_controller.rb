206fdfbc53aad396508fea3f3bec6a1fbfe2cb04
panxiubin <xiubinpan@gmail.com>
2016-10-29 20:42:26 +0800

    implement courses#new create

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,20 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.save
+    redirect_to courses_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
 end
