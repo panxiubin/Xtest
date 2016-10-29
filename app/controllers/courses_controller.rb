5a159f791c9fc675e71901b825607fa282f305b9
panxiubin <xiubinpan@gmail.com>
2016-10-29 20:52:44 +0800

    implement CRUD

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,6 +13,26 @@ class CoursesController < ApplicationController
     redirect_to courses_path
   end
 
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(course_params)
+    redirect_to courses_path, notice: "Update Success"
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to courses_path, alert: "Course deleted"
+  end
+
   private
 
   def course_params
